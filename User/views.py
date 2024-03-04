8b2a85631bf739d39a41dfa579de4f1a64f5ee08
Musoxon <orinboyevmusoxon05@gamil.com>
2024-03-04 19:49:32 +0500

    Sign Up change 1

--- User/views.py
+++ User/views.py
@@ -49,16 +49,19 @@ class SignUpView(APIView):
 
     def post(self, request):
         serializer = UserSerializer(data=request.data)
-        user = User.objects.all()
-        for i in user:
-            if i.status == 'Direktor':
-                if serializer.is_valid() and serializer.status == 'Direktor':
-                    return Response({'xabar':'Direktor oldin yaratilgan'})
-            else:
-                if serializer.is_valid():
+        
+        if serializer.is_valid():
+            if serializer.validated_data['status'] == 'Direktor':
+                # Check if there is already a 'Direktor' user
+                if User.objects.filter(status='Direktor').exists():
+                    return Response({'xabar': 'Direktor oldin yaratilgan'}, status=status.HTTP_400_BAD_REQUEST)
+                else:
                     serializer.save()
-                    return Response(serializer.data)
-        return Response(serializer.errors)
+                    return Response(serializer.data, status=status.HTTP_201_CREATED)
+            else:
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class SignUpDetail(APIView):
